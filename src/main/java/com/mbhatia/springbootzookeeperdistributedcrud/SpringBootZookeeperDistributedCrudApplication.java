04a2675b6e62542e6748019754cd48435da9225c
Munish Bhatia <mbhatia@oanda.com>
2019-10-22 16:13:03 -0400

    Initial Commit; Project Initialized

---
+++ src/main/java/com/mbhatia/springbootzookeeperdistributedcrud/SpringBootZookeeperDistributedCrudApplication.java
@@ -0,0 +1,13 @@
+package com.mbhatia.springbootzookeeperdistributedcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZookeeperDistributedCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZookeeperDistributedCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbhatia/springbootzookeeperdistributedcrud/SpringBootZookeeperDistributedCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbhatia.springbootzookeeperdistributedcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZookeeperDistributedCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
